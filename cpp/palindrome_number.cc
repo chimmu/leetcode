0bf97dc252b343443527d2477205b81ffb22acf3
chimmu <chimmuchiu@gmail.com>
2015-07-16 14:29:20 +0800

    palindrome_number

--- cpp/palindrome_number.cc
+++ cpp/palindrome_number.cc
@@ -6,7 +6,24 @@ using namespace std;
 class Solution {
 	public:
 		bool isPalindrome(int x) {
-			     			    
+			if (x < 0) return false;
+			long long rev = 0;
+			int tmp = x;
+			while (tmp > 0) {
+				rev *= 10;
+				rev += tmp % 10;
+				tmp /= 10;
+			}
+			return rev == x;
 		}
 
 };
+
+int main()
+{
+	Solution s;
+	cout << s.isPalindrome(-1) << endl;
+	cout << s.isPalindrome(2332) << endl;
+	cout << s.isPalindrome(1234321) << endl;
+	return 0;
+}
